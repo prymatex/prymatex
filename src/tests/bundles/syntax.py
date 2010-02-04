ba53e5e72f010296e7161afc9089029a3744fb03
diego@ubuntu.rw.dgrchubut.gov.ar <diego@ubuntu.rw.dgrchubut.gov.ar>
2010-02-04 10:25:46 -0300

    agregando bundles

--- src/tests/bundles/syntax.py
+++ src/tests/bundles/syntax.py
@@ -3,9 +3,53 @@
 import re
 import ipdb
 import plistlib
-#from debug_processor import DebugProcessor
 
 SYNTAXES = {}
+
+class SyntaxProcessor(object):
+    def __init__(self):
+        pass
+
+    def open_tag(self, name, position):
+        pass
+
+    def close_tag(self, name, position):
+        pass
+
+    def new_line(self, line):
+        pass
+
+    def start_parsing(self, name):
+        pass
+
+    def end_parsing(self, name):
+        pass
+
+class DebugSyntaxProcessor(SyntaxProcessor):
+    def __init__(self):
+        self.line_number = 0
+        self.printable_line = ''
+
+    def pprint(self, line, string, position = 0):
+        line = line[:position] + string + line[position:]
+        return line
+
+    def open_tag(self, name, position):
+        print self.pprint( '', '{ %s' % name, position + len(self.line_marks))
+
+    def close_tag(self, name, position):
+        print self.pprint( '', '} %s' % name, position + len(self.line_marks))
+
+    def new_line(self, line):
+        self.line_number += 1
+        self.line_marks = '[%04s] ' % self.line_number
+        print '%s%s' % (self.line_marks, line)
+
+    def start_parsing(self, name):
+        print '{%s' % name
+
+    def end_parsing(self, name):
+        print '}%s' % name
 
 class SyntaxProxy(object):
     def __init__(self, hash, syntax):
@@ -17,21 +61,17 @@ class SyntaxProxy(object):
             proxy_value = self.__proxy()
             if proxy_value:
                 return getattr(proxy_value, name)
-            #TODO: else raise exception
+        #TODO: else raise exception
     
     def __proxy(self):
         if re.compile('^#').match(self.proxy):
             return self.syntax.repository[self.proxy[1:]]
-        elif "$self":
+        elif '$self':
             return self.syntax
-        elif "$base":
+        elif '$base':
             return self.syntax
         else:
             return self.syntax.syntaxes[self.proxy]
-
-def parse_file(filename, name_space = 'default'):
-    data = plistlib.readPlist(filename)
-    return SyntaxNode(data, None, name_space)
 
 class SyntaxNode(object):
     OPTIONS = re.UNICODE
@@ -43,55 +83,56 @@ class SyntaxNode(object):
             setattr(self, k, None)
         self.name_space = name_space
         SYNTAXES.setdefault(self.name_space, {})
+        #Definicion de un scope
         if 'scopeName' in hash:
-            SYNTAXES[self.name_space][hash["scopeName"]] = self 
+            SYNTAXES[self.name_space][hash['scopeName']] = self 
         self.syntax = syntax or self
         for key, value in hash.iteritems():
-            if key in ["firstLineMatch", "foldingStartMarker", "foldingStopMarker", "match", "begin"]:
+            if key in ['firstLineMatch', 'foldingStartMarker', 'foldingStopMarker', 'match', 'begin']:
                 try:
-		    # Estos replace hay que sacarlos si usamos el motor de expreciones de la dll
+                    # Estos replace hay que sacarlos si usamos el motor de expreciones de la dll
                     value = value.replace('?i:', '(?i)')
                     value = value.replace('?x:', '(?x)')
                     value = value.replace('?<=', '(?<=)')
                     setattr(self, key, re.compile( value ))
                 except:
-		  pass
-                  #print "Parsing error in %s: %s" % (key, value)
-            elif key in ["content", "fileTypes", "name", "contentName", "end", "scopeName", "keyEquivalent"]:
+                    pass
+                #print 'Parsing error in %s: %s' % (key, value)
+            elif key in ['content', 'fileTypes', 'name', 'contentName', 'end', 'scopeName', 'keyEquivalent']:
                 setattr(self, key, value )
-            elif key in ["captures", "beginCaptures", "endCaptures"]:
+            elif key in ['captures', 'beginCaptures', 'endCaptures']:
                 keys = map(lambda v: int(v), value.keys())
-                value = map(lambda v: value["%s" % (v)], keys)
+                value = map(lambda v: value['%s' % (v)], keys)
                 setattr(self, key, value.sort() )
-            elif key == "repository":
+            elif key == 'repository':
                 self.parse_repository(value)
-            elif key in ["patterns"]:
+            elif key in ['patterns']:
                 self.create_children(value)
             else:
-		pass
-                #print u"Ignoring: %s: %s" % (key, value)
+                pass
+                #print u'Ignoring: %s: %s' % (key, value)
     
     def parse(self, string, processor = None ):
         if processor:
             processor.start_parsing(self.scopeName)
         stack = [[self, None]]
         for line in string.splitlines():
-           self.parse_line(stack, line, processor)
+            self.parse_line(stack, line, processor)
         if processor:
             processor.end_parsing(self.scopeName)
         return processor
     
     def parse_repository(self, repository):
-         self.repository = {}
-         for key, value in repository.iteritems():
+        self.repository = {}
+        for key, value in repository.iteritems():
             if 'include' in value:
-               self.repository[key] = SyntaxProxy( value, self.syntax )
+                self.repository[key] = SyntaxProxy( value, self.syntax )
             else:
-               self.repository[key] = SyntaxNode( value, self.syntax, self.name_space )
+                self.repository[key] = SyntaxNode( value, self.syntax, self.name_space )
 
     def create_children(self, patterns):
-         self.patterns = []
-         for p in patterns:
+        self.patterns = []
+        for p in patterns:
             if 'include' in p:
                 self.patterns.append(SyntaxProxy( p, self.syntax ))
             else:
@@ -99,7 +140,7 @@ class SyntaxNode(object):
     
     def parse_captures(self, name, pattern, match, processor):
         captures = pattern.match_captures( name, match )
-        #reject en ruby para python niego
+        
         captures = filter(lambda group, range, name: range[0] and range[0] != range[-1], captures)
         starts = []
         ends = []
@@ -111,16 +152,16 @@ class SyntaxNode(object):
          
         while starts or ends:
             if starts:
-                pos, key, name = ends.pop()
+                pos, _, name = ends.pop()
                 processor.close_tag(name, pos)
             elif ends:
-                pos, key, name = starts.pop
+                pos, _, name = starts.pop
                 processor.open_tag(name, pos)
             elif abs(ends[-1][1]) < starts[-1][1]:
-                pos, key, name = ends.pop()
+                pos, _, name = ends.pop()
                 processor.close_tag(name, pos)
             else:
-                pos, key, name = starts.pop()
+                pos, _, name = starts.pop()
                 processor.open_tag(name, pos)
     
     def match_captures(self, name, match):
@@ -130,10 +171,10 @@ class SyntaxNode(object):
             for key, value in captures:
                 if not re.compile('^\d*$').match(key):
                     if int(key) < len(match):
-                        matches.append([int(key), match.offset( int(key) ), value["name"]])
+                        matches.append([int(key), match.offset( int(key) ), value['name']])
                 else:
                     if match.to_index( key.to_sym ):
-                        matches.append([match.to_index( key.to_sym ), match.offset( key.to_sym), value["name"]])
+                        matches.append([match.to_index( key.to_sym ), match.offset( key.to_sym), value['name']])
         return matches
         
     def match_first(self, string, position):
@@ -146,9 +187,9 @@ class SyntaxNode(object):
             if match:
                 return (self, match) 
         elif self.end:
-           pass
+            pass
         else:
-           return self.match_first_son( string, position )
+            return self.match_first_son( string, position )
         return (None, None)
     
     def match_end(self, string, match, position):
@@ -191,50 +232,54 @@ class SyntaxNode(object):
                 end_match = top.match_end( line, match, position )
             
             if end_match and ( not pattern_match or pattern_match.start() >= end_match.start() ):
-               pattern_match = end_match
-               start_pos = pattern_match.start()
-               end_pos = pattern_match.end()
-               if top.contentName and processor:
-                   processor.close_tag(top.contentName, start_pos)
-               if processor:
-                   self.parse_captures("captures", top, pattern_match, processor)
-               if processor:
-                   self.parse_captures("endCaptures", top, pattern_match, processor)
-               if top.name and processor:
-                   processor.close_tag( top.name, end_pos)
-               stack.pop()
-               top, match = stack[-1]
+                pattern_match = end_match
+                start_pos = pattern_match.start()
+                end_pos = pattern_match.end()
+                if top.contentName and processor:
+                    processor.close_tag(top.contentName, start_pos)
+                if processor:
+                    self.parse_captures('captures', top, pattern_match, processor)
+                if processor:
+                    self.parse_captures('endCaptures', top, pattern_match, processor)
+                if top.name and processor:
+                    processor.close_tag( top.name, end_pos)
+                stack.pop()
+                top, match = stack[-1]
             else:
-               if not pattern:
-                   break 
-               start_pos = pattern_match.start()
-               end_pos = pattern_match.end()
-               if pattern.begin:
-                   if pattern.name and processor:
-                       processor.open_tag(pattern.name, start_pos)
-                   if processor:    
-                       self.parse_captures("captures", pattern, pattern_match, processor)
-                   if processor:
-                       self.parse_captures("beginCaptures", pattern, pattern_match, processor)
-                   if pattern.contentName and processor:
-                       processor.open_tag(pattern.contentName, end_pos)
-                   top = pattern
-                   match = pattern_match
-                   stack.append([top, match])
-               elif pattern.match:
-                   if pattern.name and processor:
-                       processor.open_tag(pattern.name, start_pos)
-                   if processor:
-                       self.parse_captures("captures", pattern, pattern_match, processor)
-                   if pattern.name and processor:
-                       processor.close_tag(pattern.name, end_pos)
+                if not pattern:
+                    break 
+                start_pos = pattern_match.start()
+                end_pos = pattern_match.end()
+                if pattern.begin:
+                    if pattern.name and processor:
+                        processor.open_tag(pattern.name, start_pos)
+                    if processor:    
+                        self.parse_captures('captures', pattern, pattern_match, processor)
+                    if processor:
+                        self.parse_captures('beginCaptures', pattern, pattern_match, processor)
+                    if pattern.contentName and processor:
+                        processor.open_tag(pattern.contentName, end_pos)
+                    top = pattern
+                    match = pattern_match
+                    stack.append([top, match])
+                elif pattern.match:
+                    if pattern.name and processor:
+                        processor.open_tag(pattern.name, start_pos)
+                    if processor:
+                        self.parse_captures('captures', pattern, pattern_match, processor)
+                    if pattern.name and processor:
+                        processor.close_tag(pattern.name, end_pos)
             position = end_pos
         return position
+
+def parse_file(filename, name_space = 'default'):
+    data = plistlib.readPlist(filename)
+    return SyntaxNode(data, None, name_space)
 
 if __name__ == '__main__':
     python = parse_file('./Python.tmLanguage', 'python')
     json = parse_file('./JSON.tmLanguage', 'json')
-    p = DebugProcessor()
+    p = DebugSyntaxProcessor()
     print python.parse(open('./debug_processor.py', 'r').read(), p)
     print json.parse('{"hola": "mundo", "estas": [1,2,3,"re","loco"]}', p)
     ipdb.set_trace()
--- src/tests/bundles/template.py
+++ src/tests/bundles/template.py
@@ -1,3 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+    Macro's module
+        content, name, scope, keyEquivalent, tabTrigger
+'''
+from pprint import pprint
+
+TEMPLATES = {}
+
+class Template(object):
+    def __init__(self, hash, name_space):
+        pprint(hash)
+        self.name_space = name_space
+        TEMPLATES.setdefault(self.name_space, {})
+        #Todo lo que diga scope es en que scope esta, puede ser mas de uno
+        if 'scope' in hash:
+            scopes = hash['scope'].split(', ')
+            for scope in scopes:
+                TEMPLATES[self.name_space][scope] = self 
+        self.content = hash.get('content')
+        self.name = hash.get('name')
+        self.keyEquivalent = hash.get('keyEquivalent')
+        self.tabTrigger = hash.get('tabTrigger')
