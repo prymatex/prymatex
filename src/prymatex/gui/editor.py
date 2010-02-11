ebcfb24419bc6d7bf2c1cf63b85a95decfa337ea
diegomvh <devnull@localhost>
2010-02-11 11:19:17 -0300

    Falta pero lo basico esta

--- src/prymatex/gui/editor.py
+++ src/prymatex/gui/editor.py
@@ -5,11 +5,11 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from prymatex.lib.i18n import ugettext as _
-import os
-import codecs
+import os, codecs
 from os.path import basename
 
-from prymatex.lib.textmate.syntax import TMSyntaxProcessor, TM_SYNTAXES 
+from prymatex.gui.syntax import PMXSyntaxProcessor
+from prymatex.lib.textmate.syntax import TM_SYNTAXES 
 from prymatex.lib.textmate.theme import TM_THEMES
 
 def deco(f):
@@ -19,43 +19,7 @@ def deco(f):
         print "%s() -> %s" % (f.func_name, retval)
         return retval
     return wrapped 
-
-class Token(object):
-    def __init__(self, begin, end, scopes):
-        self.begin, self.end, self.scopes = begin, end, scopes
-
-    def __str__(self):
-        return '<token: Position: (%d, %d) Scopes: "%s...">' % (self.begin, self.end, ', '.join(self.scopes))
-
-class PMXSyntaxProcessor(QSyntaxHighlighter, TMSyntaxProcessor):
-    def __init__(self, parent, syntax, style):
-        QSyntaxHighlighter.__init__(self, parent)
-        self.syntax = syntax
-        self.style = style
-    
-    def highlightBlock(self, texto):
-        stack = [[self.syntax, None]]
-        self.tokens = []
-        self.scopes = []
-        self.current_position = None
-        self.syntax.parse_line(stack, unicode(texto), self)
-
-    def open_tag(self, name, position):
-        self.scopes.append(name)
-        if self.current_position != None:
-            self.add_token(self.current_position, position, self.scopes[:])
-        self.current_position = position
-
-    def close_tag(self, name, position):
-        if self.current_position == None or name != self.scopes[-1]:
-            raise Exception('Error al parsear un token.')
-        self.add_token(self.current_position, position, self.scopes[:])
-        self.scopes.pop()
-    
-    def add_token(self, begin, end, scopes):
-        self.tokens = Token(begin, end, scopes)
-        self.setFormat(begin, end - begin, self.style.get_format(scopes[-1]))
-    
+   
 class PMXTextEdit(QPlainTextEdit):
     _path = ''
     _tab_length = 4
@@ -75,8 +39,7 @@ class PMXTextEdit(QPlainTextEdit):
         self.setTabChangesFocus(False)
         #print self.connect(self, SIGNAL("destroyed(QObject)"), self.cleanUp)
         try:
-            self.syntax_processor = PMXSyntaxProcessor(self, TM_SYNTAXES['HTML']['text.html.basic'], None)
-            self.syntax_processor.setDocument(self.document())
+            self.syntax_processor = PMXSyntaxProcessor(self.document(), TM_SYNTAXES['Python']['source.python'], TM_THEMES['iPlastic'])
         except Exception, e:
             print ("Error al cargar la sintaxis %s" % e)
         
---
+++ src/prymatex/gui/syntax.py
@@ -0,0 +1,40 @@
+from PyQt4.Qt import QSyntaxHighlighter
+
+from prymatex.lib.textmate.syntax import TMSyntaxProcessor 
+
+class Token(object):
+    def __init__(self, begin, end, scopes):
+        self.begin, self.end, self.scopes = begin, end, scopes
+
+    def __str__(self):
+        return '<token: Position: (%d, %d) Scopes: "%s...">' % (self.begin, self.end, ', '.join(self.scopes))
+
+class PMXSyntaxProcessor(QSyntaxHighlighter, TMSyntaxProcessor):
+    def __init__(self, doc, syntax, style):
+        QSyntaxHighlighter.__init__(self, doc)
+        self.syntax = syntax
+        self.style = style
+    
+    def highlightBlock(self, texto):
+        stack = [[self.syntax, None]]
+        self.tokens = []
+        self.scopes = []
+        self.current_position = None
+        self.syntax.parse_line(stack, unicode(texto), self)
+
+    def open_tag(self, name, position):
+        self.scopes.append(name)
+        if self.current_position != None:
+            self.add_token(self.current_position, position, self.scopes[:])
+        self.current_position = position
+
+    def close_tag(self, name, position):
+        if self.current_position == None or name != self.scopes[-1]:
+            raise Exception('Error al parsear un token.')
+        self.add_token(self.current_position, position, self.scopes[:])
+        self.scopes.pop()
+    
+    def add_token(self, begin, end, scopes):
+        self.tokens = Token(begin, end, scopes)
+        print "%s, %s, " % (begin, end - begin)
+        self.setFormat(begin, end - begin, self.style.get_format(scopes[-1]))
