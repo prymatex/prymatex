4cfaa9b96cb727dc0cad9cc4cc4a1fe28e11054c
defo@asgard.karmic.lan <defo@asgard.karmic.lan>
2010-02-15 00:51:28 -0300

    Cosas que faltaron

---
+++ src/prymatex/gui/panes/bundles.py
@@ -0,0 +1,45 @@
+#-*- encoding: utf-8 -*-
+
+from PyQt4.QtGui import *
+from prymatex.lib.i18n import ugettext as _
+
+class PMXBundleWidget(QWidget):
+    def __init__(self, parent):
+        QWidget.__init__(self, parent)
+        self.setupGui()
+    
+    def setupGui(self):
+        layout = QVBoxLayout()
+        self.comboSelectBundleType = QComboBox(self)
+        self.comboSelectBundleType.addItem("Syntax")
+        self.comboSelectBundleType.addItem("Snippet")
+        self.comboSelectBundleType.addItem("Command")
+        self.comboSelectBundleType.addItem("Preferences")
+        layout.addWidget(self.comboSelectBundleType)
+        layout.addWidget(QTreeView())
+        
+        
+        layoutButtons = QHBoxLayout()
+        self.buttonAdd = QPushButton(_("Add"), self)
+        self.buttonAdd.setObjectName("buttonAdd")
+        layoutButtons.addWidget(self.buttonAdd)
+        
+        self.buttonRemove = QPushButton(_("Remove"), self)
+        self.buttonRemove.setObjectName("buttonRemove")
+        layoutButtons.addWidget(self.buttonRemove)
+        
+        self.buttonManage = QPushButton(_("Manage"), self)
+        self.buttonManage.setObjectName("buttonMange")
+        layoutButtons.addWidget(self.buttonManage)
+        layoutButtons.addStretch(-1)
+        
+        layout.addLayout(layoutButtons)
+        self.setLayout(layout)
+        
+    
+class PMXBundleEditorDock(QDockWidget):
+    def __init__(self, parent):
+        QDockWidget.__init__(self, parent)
+        self.setWindowTitle(_("Bundles"))
+        self.setWidget(PMXBundleWidget(self))
+        
---
+++ src/prymatex/lib/os/fsmonitor/__init__.py
@@ -0,0 +1,27 @@
+'''
+Monitor filesystem changes to be notified to apps
+Possible backend could be:
+    - Thread, like Django does
+    - inotify on Linux systems
+    - ???
+'''
+
+class PMXFileMonitor(object):
+    pass
+
+
+from PyQt4.QtCore import QThread
+
+class PMXThreadFileMonitor(PMXFileMonitor, QThread):
+    def __init__(self, parent):
+        QThread.__init__(self, parent)
+        PMXFileMonitor.__init__(self)
+        
+    def addPath(self, filepath, filter):
+        pass
+    
+    
+    
+    
+    
+    
