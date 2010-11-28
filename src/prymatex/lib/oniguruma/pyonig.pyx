28eba4dd82faf8a05b5f30fd82fc40d6e61d0f84
Nahuel <nahuel.defosse@gmail.com>
2010-11-27 21:02:43 -0300

    Ahora se compila la expresion regular, falta recuperar los matches

--- src/prymatex/lib/oniguruma/pyonig.pyx
+++ src/prymatex/lib/oniguruma/pyonig.pyx
@@ -15,21 +15,39 @@ from oniguruma_cdefs cimport *
 
 from libc cimport stdlib
 
-class Regex:
+
+    
+
+cdef class Regex:
     '''
     Oniguruma Regex
     '''
-    def __init__(self, char *pattern, int options = ONIG_OPTION_NONE):
-        cdef regex_t *c_oregexp 
+    cdef regex_t *oregexp
+    cdef public char* pattern
+    cdef public int options
+
+    def __init__(self, char *pattern, int options = OPTION_NONE):
+        '''
+        Creates a regular expression instance. 
+        '''
+        
         self.pattern = pattern
         self.options = options
         cdef UChar* pat_ptr = pattern
+        cdef OnigErrorInfo einfo 
+        
+        r = onig_new( &self.oregexp, pattern, self.pattern + len(pattern),
+                        options, &OnigEncodingASCII, &OnigSyntaxASIS, &einfo)
+        print "El resultado fue %d" % r
+        print "Bytes alojados: %d Bytes usados: %d" % ( self.oregexp.alloc, 
+                                                        self.oregexp.used)
     
     def match(self, s):
         '''
         Match. Should return something iterable
         '''
-        pass 
+        cdef OnigRegion *region
+        
         
     def __str__(self):
         return "<ORegex %s>" % self.pattern[:20]
@@ -38,4 +56,5 @@ class Regex:
 
 # Module initialization
 VERSION = onig_version()
+
 
