b923ad012dd98091a08b734853a712bf336f2aec
Nahuel <nahuel.defosse@gmail.com>
2011-01-05 12:58:14 -0300

    unicode in i18n

--- src/prymatex/lib/i18n.py
+++ src/prymatex/lib/i18n.py
@@ -1,13 +1,18 @@
+'''
+Translate strings
+'''
 
+from PyQt4 import QtGui
+from PyQt4 import QtCore
 
-from PyQt4.QtGui import QApplication
-
-def ugettext(string, *largs, **kwargs):
-    if not isinstance(string, basestring):
-        return string
+def ugettext(source, *largs, **kwargs):
+    ''' Translate a string '''
+    assert isinstance(source, (basestring, QtCore.QString))
+    qapp = QtGui.qApp.instance()
+    i18n_string = qapp.trUtf8(qapp.tr(source))
     
-    i18n_string = string
     if i18n_string.count('%') and largs:
-        i18n_string = i18n_string % largs
-    return i18n_string
+        i18n_string = unicode(i18n_string) % largs
     
+    return i18n_string
+
