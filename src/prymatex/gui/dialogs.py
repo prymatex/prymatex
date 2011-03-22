ccd0b1d8432610a7e4143f19b67472354630c119
Nahuel <nahuel.defosse@gmail.com>
2011-03-22 13:58:56 -0300

    Some minor bugfixes to the New From Tempaltes

--- src/prymatex/gui/dialogs.py
+++ src/prymatex/gui/dialogs.py
@@ -1,7 +1,9 @@
 
 from PyQt4.Qt import QDialog, QVBoxLayout, QPushButton, QFileDialog, QVariant
-from PyQt4.QtCore import pyqtSignal
+from PyQt4.QtCore import pyqtSignal, QDir
 from PyQt4.Qt import SIGNAL
+from PyQt4.QtGui import QCompleter, QFileSystemModel, QMessageBox
+from os.path import isdir
 from prymatex.bundles import PMXBundle
 from prymatex.gui.ui_multiclose import Ui_SaveMultipleDialog
 from prymatex.gui.ui_newtemplate import Ui_NewFromTemplateDialog
@@ -26,23 +28,58 @@ class NewFromTemplateDialog(QDialog, Ui_NewFromTemplateDialog):
     def __init__(self, parent):
         super(NewFromTemplateDialog, self).__init__(parent)
         self.setupUi(self)
+        model = QFileSystemModel(self)
+        model.setRootPath("")
+        model.setFilter(QDir.Dirs)
+        self.completerFileSystem = QCompleter(model, self)
+        self.lineLocation.setCompleter(self.completerFileSystem)
         for template in PMXBundle.TEMPLATES:
             self.comboTemplates.addItem(template.name, userData = QVariant(template))
     
     def on_buttonChoose_pressed(self):
         path = QFileDialog.getExistingDirectory(self, self.trUtf8("Choose Location for Template"))
-        self.lineLocation.setText(path)
+        if path:
+            self.lineLocation.setText(path)
 
     def on_buttonCreate_pressed(self):
         #TODO: Validar que los lineEdit tengan texto
         template = self.comboTemplates.itemData(self.comboTemplates.currentIndex()).toPyObject()
         environment = template.buildEnvironment(directory = unicode(self.lineLocation.text()), name = unicode(self.lineFileName.text()))
+        if not template:
+            QMessageBox.question(self, "Information requiered", 
+                                 "You did not suply all the information required "
+                                 "for the file", 
+                                 buttons=QMessageBox.Cancel | QMessageBox.Retry, 
+                                 defaultButton=QMessageBox.NoButton)
         template.resolve(environment)
         self.newFileCreated.emit(environment['TM_NEW_FILE'])
         self.close()
-
+    
+    def check_valid_location(self):
+        ''' Disable file '''
+        location = unicode(self.lineLocation.text())
+        if not isdir(location):
+            self.buttonCreate.setEnabled(False)
+            return
+        filename = unicode(self.lineFileName)
+        if not filename:
+            self.buttonCreate.setEnabled(False)
+            return
+        self.buttonCreate.setEnabled(True)
+             
+    def on_lineFileName_textChanged(self, text):
+        self.check_valid_location()
+    
+    def on_lineLocation_textChanged(self, text):
+        self.check_valid_location()
+            
     def on_buttonClose_pressed(self):
         self.close()
+    
+    def exec_(self):
+        self.lineFileName.setText('')
+        self.check_valid_location()
+        super(NewFromTemplateDialog, self).exec_()
         
 def test():
     def multiclose_dialog(p):
