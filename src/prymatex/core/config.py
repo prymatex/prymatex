c8f585c80c26412c9e8caf52c8e81de68578e7d6
Nahuel <nahuel.defosse@gmail.com>
2011-04-15 23:56:55 -0300

    Reduce module path in config

--- src/prymatex/core/config.py
+++ src/prymatex/core/config.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python
 #-*- encoding: utf-8 -*-
 from PyQt4.Qt import QSettings
-import os
+from os.path import join, abspath, expanduser, dirname, exists
+from os import makedirs
 
 PRYMATEX_HOME_NAME = ".prymatex"
 PRYMATEX_SETTING_NAME = "settings.ini"
 
 def get_prymatex_base_path():
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    return abspath(join(dirname(__file__), '..'))
 
 def get_prymatex_user_path():
-    path = os.path.abspath(os.path.join(os.path.expanduser("~"), PRYMATEX_HOME_NAME))
-    if not os.path.exists(path):
-        os.makedirs(path)
+    path = abspath(join(expanduser("~"), PRYMATEX_HOME_NAME))
+    if not exists(path):
+        makedirs(path)
     return path
 
 def build_prymatex_profile(path):
-    os.makedirs(path)
-    os.makedirs(os.path.join(path, 'tmp'))
-    os.makedirs(os.path.join(path, 'log'))
+    makedirs(path)
+    makedirs(join(path, 'tmp'))
+    makedirs(join(path, 'log'))
     
 def get_prymatex_profile_path(name, base):
-    path = os.path.abspath(os.path.join(base, name.lower()))
-    if not os.path.exists(path):
+    path = abspath(join(base, name.lower()))
+    if not exists(path):
         build_prymatex_profile(path)
     return path
 
@@ -30,9 +31,9 @@ def get_prymatex_profile_path(name, base):
 PMX_BASE_PATH = get_prymatex_base_path()
 PMX_USER_PATH = get_prymatex_user_path()
 PMX_APP_PATH = PMX_BASE_PATH
-PMX_BUNDLES_PATH = os.path.join(PMX_BASE_PATH, 'share', 'Bundles')
-PMX_THEMES_PATH = os.path.join(PMX_BASE_PATH, 'share', 'Themes')
-PMX_SUPPORT_PATH = os.path.join(PMX_BASE_PATH, 'share', 'Support')
+PMX_BUNDLES_PATH = join(PMX_BASE_PATH, 'share', 'Bundles')
+PMX_THEMES_PATH = join(PMX_BASE_PATH, 'share', 'Themes')
+PMX_SUPPORT_PATH = join(PMX_BASE_PATH, 'share', 'Support')
 
 class SettingsGroup(object):
     def __init__(self, name, qsettings):
@@ -119,22 +120,22 @@ class pmxConfigPorperty(object):
 class PMXSettings(object):
     PMX_APP_PATH = get_prymatex_base_path()
     PMX_USER_PATH = get_prymatex_user_path()
-    PMX_BUNDLES_PATH = os.path.join(PMX_APP_PATH, 'share', 'Bundles')
-    PMX_THEMES_PATH = os.path.join(PMX_APP_PATH, 'share', 'Themes')
-    PMX_SUPPORT_PATH = os.path.join(PMX_APP_PATH, 'share', 'Support')
+    PMX_BUNDLES_PATH = join(PMX_APP_PATH, 'share', 'Bundles')
+    PMX_THEMES_PATH = join(PMX_APP_PATH, 'share', 'Themes')
+    PMX_SUPPORT_PATH = join(PMX_APP_PATH, 'share', 'Support')
     #Profile
     PMX_PROFILE_PATH = None
     PMX_TMP_PATH = None
     PMX_LOG_PATH = None
     GROUPS = {}
     def __init__(self, profile_path):
-        self.qsettings = QSettings(os.path.join(profile_path, PRYMATEX_SETTING_NAME), QSettings.IniFormat)
+        self.qsettings = QSettings(join(profile_path, PRYMATEX_SETTING_NAME), QSettings.IniFormat)
     
     @classmethod
     def getSettingsForProfile(cls, profile):
         cls.PMX_PROFILE_PATH = get_prymatex_profile_path(profile, cls.PMX_USER_PATH)
-        cls.PMX_TMP_PATH = os.path.join(cls.PMX_PROFILE_PATH, 'tmp')
-        cls.PMX_LOG_PATH = os.path.join(cls.PMX_PROFILE_PATH, 'log')
+        cls.PMX_TMP_PATH = join(cls.PMX_PROFILE_PATH, 'tmp')
+        cls.PMX_LOG_PATH = join(cls.PMX_PROFILE_PATH, 'log')
         return cls(cls.PMX_PROFILE_PATH)
     
     def getGroup(self, name):
