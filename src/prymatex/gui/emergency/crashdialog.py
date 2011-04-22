2465301770dcd1931a8cf25f9191216aed48f3dc
Nahuel <nahuel.defosse@gmail.com>
2011-04-22 17:36:20 -0300

    Write exception information to stderr while shutting down

--- src/prymatex/gui/emergency/crashdialog.py
+++ src/prymatex/gui/emergency/crashdialog.py
@@ -1,16 +1,20 @@
 from PyQt4.Qt import *
-
 from ui_crash import Ui_CrashDialog
-
+import sys
 
 class PMXCrashDialog(QDialog, Ui_CrashDialog):
     '''
         Emergency Dialog
     '''
-    def __init__(self, traceback_text):
+    
+    def __init__(self, traceback_text, show_through_stderr = True):
         super(PMXCrashDialog, self).__init__()
         self.setupUi(self)
-        self.textEdit.setText(unicode(traceback_text))
+        text = unicode(traceback_text)
+        self.textEdit.setText(text)
+        if show_through_stderr:
+            sys.stderr.write(text)
+            sys.stderr.flush()
     
     def on_pushCopyTraceback_pressed(self):
         clipboard = qApp.instance().clipboard()
