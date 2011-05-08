e4c0904ee49f2cc037fc73dca611693996f43e38
Nahuel <nahuel.defosse@gmail.com>
2011-05-08 15:42:51 -0300

    Select bundle item

--- src/prymatex/gui/bundles/tableview.py
+++ src/prymatex/gui/bundles/tableview.py
@@ -1,4 +1,5 @@
 from PyQt4.Qt import *
+from prymatex.core.base import PMXObject
 
 
 class PMXBundleItemTableView(QTableView):
@@ -31,7 +32,9 @@ class PMXBundleItemTableView(QTableView):
 
 
 class PMXFilterBundleItem(QSortFilterProxyModel):
-    
+    '''
+    Filters from user input
+    '''
     #def filterAcceptsRow(self, sourceRow, parent):
         #print sourceRow
     #    return True
@@ -40,13 +43,33 @@ class PMXFilterBundleItem(QSortFilterProxyModel):
         print value
     
 
-class PMXBundleItemSelector(QDialog):
+class PMXBundleItemSelector(QDialog, PMXObject):
+    '''
+    This dialog allow the user to search through commands and snippets
+    easily. An instance is hold in the main window and triggered with
+    an action. 
+    '''
     def __init__(self, parent = None):
+        
         super(PMXBundleItemSelector, self).__init__(parent)
         self.setupUi()
         #self.hideUnusedColumns()
         self.setModal(True)
         self.lineFilter.textChanged.connect(self.proxyFilteringModel.setFilteringProxy)
+    
+    def exec_(self):
+        # Geo is not known until the mainwindow is shown, so we 
+        # must accept it
+        geo = self.mainwindow.geometry()
+        w  = geo.width() * .8
+        h = geo.height() * .4
+        geo.setX(geo.x() + (geo.width() - w)/2)
+        geo.setY(geo.y() + (geo.height() - h)/2)
+        geo.setWidth(w)
+        geo.setHeight(h)
+        self.setGeometry(geo)
+        self.lineFilter.setFocus()
+        super(PMXBundleItemSelector, self).exec_()
         
     def hideUnusedColumns(self):
         model = self.tableView.model()
@@ -62,6 +85,7 @@ class PMXBundleItemSelector(QDialog):
         
         layout = QVBoxLayout()
         layout.setSpacing(0)
+        layout.setMargin(0)
         self.lineFilter = QLineEdit(self)
         layout.addWidget(self.lineFilter)
         self.tableView = QTableView(self)
