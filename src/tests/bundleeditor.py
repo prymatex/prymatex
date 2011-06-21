76c4cc088d1cd3e7b70ac0ed49207ff6e1e04c41
diegomvh <diegomvh@gmail.com>
2011-06-21 10:04:44 -0300

    new path for bundleeditor test

--- src/tests/bundleeditor.py
+++ src/tests/bundleeditor.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     app.options = options
     from prymatex.gui.support.manager import PMXSupportModelManager
     app.supportManager = PMXSupportModelManager()
-    app.supportManager.addNamespace('prymatex', os.path.abspath('../bundles/prymatex'))
+    app.supportManager.addNamespace('prymatex', os.path.abspath('../prymatex/share'))
     app.supportManager.addNamespace('user', os.path.abspath(os.path.join(os.path.expanduser('~'), '.prymatex')))
     app.supportManager.loadSupport()
     from prymatex.gui.support.bundleeditor import PMXBundleEditor
