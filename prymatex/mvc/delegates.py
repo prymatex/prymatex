089c96544a0fb9f3332dc2e9206355513b0e5098
Nahuel <nahuel.defosse@gmail.com>
2011-07-06 11:18:27 -0300

    Color is not set if dialog is not accepted

--- prymatex/mvc/delegates.py
+++ prymatex/mvc/delegates.py
@@ -2,16 +2,36 @@
 # -*- coding: utf-8 -*-
 
 from PyQt4 import QtGui, QtCore
+from PyQt4.QtGui import QColorDialog
 
+class AcceptableColorDialog(QColorDialog):
+    def __init__(self, *largs):
+        super(AcceptableColorDialog, self).__init__(*largs)
+        
+    __isAccepted = False
+    @property
+    def isAccepted(self):
+        return self.__isAccepted
+    
+    @isAccepted.setter
+    def isAccepted(self, value):
+        assert type(value) is bool
+        self.__isAccepted = value
+    
+    def accept(self, *largs):
+        self.isAccepted = True
+        super(AcceptableColorDialog, self).accept(*largs)
+    
 class PMXColorDelegate(QtGui.QItemDelegate):
     def createEditor(self, parent, options, index):
-        editor = QtGui.QColorDialog(parent)
+        editor = AcceptableColorDialog(parent)
         editor.setOptions(QtGui.QColorDialog.ShowAlphaChannel)
         return editor
     
     def setModelData(self, colorDialog, model, index):
-        color = colorDialog.currentColor()
-        model.setData(index, color)
+        if colorDialog.isAccepted:
+            color = colorDialog.currentColor()
+            model.setData(index, color)
     
     def setEditorData(self, colorDialog, index):
         variant = index.data()
