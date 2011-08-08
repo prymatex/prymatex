07dcfb0246da3d0d962380d9863699eec3a84760
Nahuel <nahuel.defosse@gmail.com>
2011-08-08 01:51:25 -0300

    RPC Server port allocation

--- prymatex/core/rpcserver.py
+++ prymatex/core/rpcserver.py
@@ -43,15 +43,34 @@ class PMXCommandDispatcher(PMXObject):
         print "debug: ", options, args
         return plistlib.writePlistToString({"selectedIndex": 1})
 
-class PMXXMLRPCServerThread(QThread):
+class PMXXMLRPCServerThread(QThread, PMXObject):
     def __init__(self, parent):
         super(PMXXMLRPCServerThread, self).__init__(parent)
-        self.server = SimpleXMLRPCServer(('', PORT),
-                                         encoding = 'utf-8')
+        self.server = self.getServer(PORT, PORT + 10)
         self.dispatcher = PMXCommandDispatcher()
         self.server.register_instance(self.dispatcher)
     
+    
+    def getServer(self, start_port, end_port):
+        '''
+        @param start_port: Start of port range where servers should be listening
+        @param start_port: End of port range where servers should be listening
+        @return: A SimpleXMLRPCServer instance listening on a port within range
+        @raise OSError: If no port available within range
+        '''
+         
+        for port in range(start_port, end_port):
+            try:
+                server = SimpleXMLRPCServer(('', port), encoding = 'utf-8')
+            except Exception as e:
+                self.warn("XMLRPC Server creation error «%s»" % e)
+            else:
+                self.info("XMLRPC Server litening on %d" % port)
+                return server 
+        raise # No ports left
+        
     def run(self):
         self.server.serve_forever()
 
+    
     
