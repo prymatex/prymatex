a28e7e8c8e17a5bc18881486d977a61ee5d49c1d
diegomvh <diegomvh@gmail.com>
2012-01-18 22:33:18 -0300

    test styles

--- prymatex/gui/style.py
+++ prymatex/gui/style.py
@@ -5,48 +5,47 @@ from PyQt4 import QtCore, QtGui
 
 from prymatex import resources
 
-class PrymatexStyle(QtGui.QStyle):
+class PrymatexStyle(QtGui.QCommonStyle):
     def __init__(self):
-        QtGui.QStyle.__init__(self)
+        QtGui.QCommonStyle.__init__(self)
         self.icon = resources.getIcon("bundle")
-        defaultStyle = QtGui.QApplication.style().objectName()
-        self.style = QtGui.QStyleFactory.create(defaultStyle)
+        self.defaultStyle = QtGui.QStyleFactory.create(QtGui.QApplication.style().objectName())
     
     def combinedLayoutSpacing(self, *args, **kwargs):
-        return self.style.combinedLayoutSpacing(*args, **kwargs)
+        return self.defaultStyle.combinedLayoutSpacing(*args, **kwargs)
     
     def styleHint(self, *args, **kwargs):
-        return self.style.styleHint(*args, **kwargs)
+        return self.defaultStyle.styleHint(*args, **kwargs)
     
     def subElementRect(self, *args, **kwargs):
-        return self.style.subElementRect(*args, **kwargs)
+        return self.defaultStyle.subElementRect(*args, **kwargs)
     
     def pixelMetric(self, *args, **kwargs):
-        return self.style.pixelMetric(*args, **kwargs)
+        return self.defaultStyle.pixelMetric(*args, **kwargs)
         
     def sizeFromContents(self, *args, **kwargs):
-        return self.style.sizeFromContents(*args, **kwargs)
+        return self.defaultStyle.sizeFromContents(*args, **kwargs)
 
     def subControlRect(self, *args, **kwargs):
-        return self.style.subControlRect(*args, **kwargs)
+        return self.defaultStyle.subControlRect(*args, **kwargs)
 
     def standardPixmap(self, *args, **kwargs):
-        return self.style.standardPixmap(*args, **kwargs)
+        return self.defaultStyle.standardPixmap(*args, **kwargs)
 
     def hitTestComplexControl(self, *args, **kwargs):
-        return self.style.hitTestComplexControl(*args, **kwargs)
+        return self.defaultStyle.hitTestComplexControl(*args, **kwargs)
     
     def drawItemText(self, *args, **kwargs):
-        return self.style.drawItemText(*args, **kwargs)
+        return self.defaultStyle.drawItemText(*args, **kwargs)
     
     def drawItemPixmap(self, *args, **kwargs):
-        return self.style.drawItemPixmap(*args, **kwargs)
+        return self.defaultStyle.drawItemPixmap(*args, **kwargs)
     
     def drawPrimitive(self, *args, **kwargs):
-        return self.style.drawPrimitive(*args, **kwargs)
+        return self.defaultStyle.drawPrimitive(*args, **kwargs)
     
     def drawComplexControl(self, *args, **kwargs):
-        return self.style.drawComplexControl(*args, **kwargs)
+        return self.defaultStyle.drawComplexControl(*args, **kwargs)
     
     def drawControl(self, element, option, painter, widget):
         if (element == QtGui.QStyle.CE_DockWidgetTitle):
@@ -61,35 +60,35 @@ class PrymatexStyle(QtGui.QStyle):
             
             painter.drawPixmap(point, self.icon.pixmap(width, width))
             option.rect = option.rect.adjusted(width, 0, 0, 0)
-        self.style.drawControl(element, option, painter, widget)
+        self.defaultStyle.drawControl(element, option, painter, widget)
     
     def generatedIconPixmap(self, *args, **kwargs):
-        return self.style.generatedIconPixmap(*args, **kwargs)
+        return self.defaultStyle.generatedIconPixmap(*args, **kwargs)
     
     def itemPixmapRect(self, *args, **kwargs):
-        return self.style.itemPixmapRect(*args, **kwargs)
+        return self.defaultStyle.itemPixmapRect(*args, **kwargs)
     
     def itemTextRect(self, *args, **kwargs):
-        return self.style.itemTextRect(*args, **kwargs)
+        return self.defaultStyle.itemTextRect(*args, **kwargs)
     
     def layoutSpacing(self, *args, **kwargs):
-        return self.style.layoutSpacing(*args, **kwargs)
+        return self.defaultStyle.layoutSpacing(*args, **kwargs)
     
     def layoutSpacingImplementation(self, *args, **kwargs):
-        return self.style.layoutSpacingImplementation(*args, **kwargs)
+        return self.defaultStyle.layoutSpacingImplementation(*args, **kwargs)
 
     def polish(self, *args, **kwargs):
-        return self.style.polish(*args, **kwargs)
+        return self.defaultStyle.polish(*args, **kwargs)
     
     def standardIcon(self, *args, **kwargs):
-        return self.style.standardIcon(*args, **kwargs)
+        return self.defaultStyle.standardIcon(*args, **kwargs)
     
     def standardIconImplementation(self, *args, **kwargs):
-        return self.style.standardIconImplementation(*args, **kwargs)
+        return self.defaultStyle.standardIconImplementation(*args, **kwargs)
     
     def standardPalette(self, *args, **kwargs):
-        return self.style.standardPalette(*args, **kwargs)
+        return self.defaultStyle.standardPalette(*args, **kwargs)
     
     def unpolish(self, *args, **kwargs):
-        return self.style.unpolish(*args, **kwargs)
+        return self.defaultStyle.unpolish(*args, **kwargs)
     
