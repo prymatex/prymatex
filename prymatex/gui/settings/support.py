85c557c5de16fc892f504b4dc9a3c75940c5aaad
Nahuel Defosse <nahuel.defosse@gmail.com>
2012-02-28 11:16:56 -0300

    More UI, no functionality yet

--- prymatex/gui/settings/support.py
+++ prymatex/gui/settings/support.py
@@ -5,12 +5,13 @@ from PyQt4 import QtGui, QtCore
 from prymatex.gui.settings.models import PMXSettingTreeNode
 from prymatex.utils.i18n import ugettext as _
 from prymatex import resources
+from prymatex.ui.configure.support import Ui_Support
 
-class PMXSupportSettings(QtGui.QWidget, PMXSettingTreeNode, ):
+class PMXSupportSettings(QtGui.QWidget, PMXSettingTreeNode, Ui_Support):
     ICON = resources.getIcon('gear') 
     TITLE = "Support"
     def __init__(self, settingGroup, parent = None):
         QtGui.QWidget.__init__(self, parent)
         PMXSettingTreeNode.__init__(self, "support", settingGroup)
         #self.setupUi(self)    
-        print "Support" + "*" * 40
+        self.setupUi(self)
---
+++ prymatex/ui/configure/support.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'resources/ui/configure/support.ui'
+#
+# Created: Tue Feb 28 11:14:52 2012
+#      by: PyQt4 UI code generator 4.9.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from prymatex.utils.i18n import ugettext as _
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    _fromUtf8 = lambda s: s
+
+class Ui_Support(object):
+    def setupUi(self, Support):
+        Support.setObjectName(_fromUtf8("Support"))
+        Support.resize(400, 300)
+        self.verticalLayout = QtGui.QVBoxLayout(Support)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.label = QtGui.QLabel(Support)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.verticalLayout.addWidget(self.label)
+        self.tableView = QtGui.QTableView(Support)
+        self.tableView.setObjectName(_fromUtf8("tableView"))
+        self.verticalLayout.addWidget(self.tableView)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.pushButton = QtGui.QPushButton(Support)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/actions/run-build-install-root.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton.setIcon(icon)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.pushButton_2 = QtGui.QPushButton(Support)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap(_fromUtf8(":/icons/actions/edit-delete.png")), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.pushButton_2.setIcon(icon1)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.retranslateUi(Support)
+        QtCore.QMetaObject.connectSlotsByName(Support)
+
+    def retranslateUi(self, Support):
+        Support.setWindowTitle(_('Support Settings'))
+        self.label.setText(_('Dynamic shebang patching'))
+        self.pushButton.setText(_('Add'))
+        self.pushButton_2.setText(_('Remove'))
+
+from prymatex import resources_rc
