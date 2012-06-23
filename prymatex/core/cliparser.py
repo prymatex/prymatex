caeb521963b8beb15d300ce19436eb61278339e5
Nahuel Defosse <nahuel.defosse@gmail.com>
2012-06-23 10:08:22 -0300

    Argparse fallback for simpler cliparse

--- prymatex/core/cliparser.py
+++ prymatex/core/cliparser.py
@@ -18,113 +18,45 @@ Check project page at %s""" % prymatex.__url__
 
 try:
     import argparse
+except ImportError: # Python < 2.7
+    from prymatex.utils import argparse
 
-    new_parser = True
+parser = argparse.ArgumentParser(usage=usage, 
+    description = description,
+    version = version,
+    epilog = epilog)
 
-    def _get_parser():
-        global usage
-        global epilog
-        global version
-        global description
+parser.add_argument('file', metavar='file', type=unicode,
+    nargs='*', help='A file/s to edit', default=[])
+parser.add_argument('-f', '--files', metavar='file', type=unicode,
+    nargs='+', help='A file/s to edit', default=[])
 
-        parser = argparse.ArgumentParser(usage=usage, 
-            description = description,
-            version = version,
-            epilog = epilog)
+# Reverts custom options
+parser.add_argument('--reset-settings', metavar='reste_settings', default = False, 
+                    help = 'Restore default settings for selected profile')
 
-        parser.add_argument('file', metavar='file', type=unicode,
-            nargs='*', help='A file/s to edit', default=[])
-        parser.add_argument('-f', '--files', metavar='file', type=unicode,
-            nargs='+', help='A file/s to edit', default=[])
+parser.add_argument('-p', '--profile', metavar='profile', nargs="?", default="",
+                help = "Change profile")
+
+# Maybe useful for some debugging information
+parser.add_argument('-d','--devel', default=False, action='store_true',
+                help = 'Enable developer mode. Useful for plugin developers.')
+
+parser.add_argument('--verbose', default=0, type=int,
+                help = 'Set verbose level from 0 to 4.')
+
+parser.add_argument('--log-pattern', default='', type=str,
+                help = 'Set filter pattern for logging')
+
         
-        # Reverts custom options
-        parser.add_argument('--reset-settings', metavar='reste_settings', default = False, 
-                            help = 'Restore default settings for selected profile')
-
-        parser.add_argument('-p', '--profile', metavar='profile', nargs="?", default="",
-                        help = "Change profile")
-
-        # Maybe useful for some debugging information
-        parser.add_argument('-d','--devel', default=False, action='store_true',
-                        help = 'Enable developer mode. Useful for plugin developers.')
-
-        parser.add_argument('--verbose', default=0, type=int,
-                        help = 'Set verbose level from 0 to 4.')
-        
-        parser.add_argument('--log-pattern', default='', type=str,
-                        help = 'Set filter pattern for logging')
-        
-        
-        return parser
-
-except:
-    import optparse
-
-    new_parser = False
-
-    def _resolve_nargs(*opts):
-        final_nargs = 1
-        for opt in opts:
-            nargs = 0
-            try:
-                start = sys.argv.index(opt) + 1
-                for idx, arg in enumerate(sys.argv[start:]):
-                    if str(arg).startswith("-"):
-                        break
-                    nargs += 1
-                return nargs
-            except ValueError:
-                nargs = 1
-            if final_nargs < nargs:
-                final_nargs = nargs
-        return final_nargs
-
-    def _get_parser():
-        global usage
-        global epilog
-        global version
-        global description
-
-        parser = optparse.OptionParser(usage=usage, 
-            description = description,
-            version = version,
-            epilog = epilog)
-        
-        parser.add_option("-f", "--file",
-            type="string",
-            action="store",
-            dest="file",
-            default=[],
-            help="A file/s to edit",
-            nargs=_resolve_nargs("-f", "--file"))
-        
-        # Reverts custom options
-        parser.add_option('--reset-settings', dest='reste_settings', action = 'store_true', default = False, 
-                            help = 'Restore default settings for selected profile')
-        parser.add_option('-p', '--profile', dest='profile', default = 'default',
-                          help = "Change profile")
-
-        # Maybe useful for some debugging information
-        parser.add_option('--devel', dest='devel', action='store_true', default=False,
-                          help = 'Enable developer mode. Useful for plugin developers.')
-        
-        parser.add_option('-v', '--verbose', dest='verbose', default=0,
-                        help = 'Set verbose level from 0 to 4.')
-        
-        parser.add_option('--log-pattern', dest='log_pattern', default='',
-                        help = 'Set filter pattern for logging')
-
-        return parser
 
 def parse():
     filenames = None
     projects_path = None
     extra_plugins = None
     try:
-        if new_parser:
-            opts = _get_parser().parse_args()
-        else:
-            opts = _get_parser().parse_args()[0]
+        
+        opts = parser.parse_args()
 
         filenames = opts.file \
             if isinstance(opts.file, list) \
@@ -133,6 +65,6 @@ def parse():
             if hasattr(opts, 'files') \
             else []
     except Exception, reason:
-        print "Args couldn't be parsed."
+        print "Arguments couldn't be parsed."
         print reason
     return opts, filenames
