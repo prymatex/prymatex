41882a7c1ec51ebe2854670afc70c10d91c761b9
diegomvh <diegomvh@gmail.com>
2012-09-14 06:33:56 -0300

    new helpers?

--- prymatex/gui/dockers/helpers.py
+++ prymatex/gui/dockers/helpers.py
@@ -9,3 +9,22 @@ class RefreshHelper(PMXBaseDockKeyHelper):
     KEY = QtCore.Qt.Key_F5
     def execute(self, event):
         print "actualizar"
+        
+class CopyHelper(PMXBaseDockKeyHelper):
+    KEY = QtCore.Qt.Key_C
+    def accept(self, event):
+        print "test c", bool(event.modifiers() & QtCore.Qt.ControlModifier)
+        return bool(event.modifiers() & QtCore.Qt.ControlModifier)
+        
+    def execute(self, event):
+        print "copy"
+        
+class PasteHelper(PMXBaseDockKeyHelper):
+    KEY = QtCore.Qt.Key_V
+    def accept(self, event):
+        print "test v", bool(event.modifiers() & QtCore.Qt.ControlModifier)
+        return bool(event.modifiers() & QtCore.Qt.ControlModifier)
+        
+    def execute(self, event):
+        print "paste"
+        
