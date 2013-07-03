370362856866882cb2279ce547c953241e6d56ac
diegomvh <diegomvh@gmail.com>
2013-07-03 20:12:49 -0300

    Add django bundle

--- prymatex/share/Bundles/Python Django.tmbundle/Support/lib/run_shell.rb
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env ruby
-
-Dir.chdir ENV['TM_PROJECT_DIRECTORY']
-
-command = [ENV["TM_PYTHON"] || "python", "-u", "manage.py"] + ($* or "shell")
-
-%x{terminal.py clear; cd \"${TM_PROJECT_DIRECTORY}\"; #{command.join(' ')}}
