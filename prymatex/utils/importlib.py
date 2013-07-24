34327c7428b49d9a47a126d3be28a0caad43969b
diegomvh <diegomvh@gmail.com>
2013-07-24 11:08:16 -0300

    reloading modules

--- prymatex/utils/importlib.py
+++ prymatex/utils/importlib.py
@@ -3,6 +3,79 @@
 
 import sys
 import imp
+try:
+    import builtins
+except ImportError as ex:
+    import __builtin__ as builtins
+
+_baseimport = builtins.__import__
+_dependencies = dict()
+_parent = None
+
+def _import(name, globals=None, locals=None, fromlist=None, level=-1):
+    # Track our current parent module.  This is used to find our current
+    # place in the dependency graph.
+    global _parent
+    parent = _parent
+    _parent = name
+
+    # Perform the actual import using the base import function.
+    m = _baseimport(name, globals, locals, fromlist, level)
+
+    # If we have a parent (i.e. this is a nested import) and this is a
+    # reloadable (source-based) module, we append ourself to our parent's
+    # dependency list.
+    if parent is not None and hasattr(m, '__file__'):
+        l = _dependencies.setdefault(parent, [])
+        l.append(m)
+
+    # Lastly, we always restore our global _parent pointer.
+    _parent = parent
+
+    return m
+
+def get_dependencies(m):
+    """Get the dependency list for the given imported module."""
+    return _dependencies.get(m.__name__, None)
+
+def _reload(m, visited):
+    """Internal module reloading routine."""
+    name = m.__name__
+
+    # Start by adding this module to our set of visited modules.  We use
+    # this set to avoid running into infinite recursion while walking the
+    # module dependency graph.
+    visited.add(m)
+
+    # Start by reloading all of our dependencies in reverse order.  Note
+    # that we recursively call ourself to perform the nested reloads.
+    deps = _dependencies.get(name, None)
+    if deps is not None:
+        for dep in reversed(deps):
+            if dep not in visited:
+                _reload(dep, visited)
+
+    # Clear this module's list of dependencies.  Some import statements
+    # may have been removed.  We'll rebuild the dependency list as part
+    # of the reload operation below.
+    try:
+        del _dependencies[name]
+    except KeyError:
+        pass
+
+    # Because we're triggering a reload and not an import, the module
+    # itself won't run through our _import hook.  In order for this
+    # module's dependencies (which will pass through the _import hook) to
+    # be associated with this module, we need to set our parent pointer
+    # beforehand.
+    global _parent
+    _parent = name
+
+    # Perform the reload operation.
+    imp.reload(m)
+
+    # Reset our parent pointer.
+    _parent = None
 
 def _resolve_name(name, package, level):
     """Return the absolute name of the module to be imported."""
@@ -16,6 +89,11 @@ def _resolve_name(name, package, level):
             raise ValueError("attempted relative import beyond top-level package")
     return "%s.%s" % (package[:dot], name)
 
+def reload_module(m):
+    """Reload an existing module.
+
+    Any known dependencies of the module will also be reloaded."""
+    _reload(m, set())
 
 def import_module(name, package=None):
     """Import a module.
@@ -23,8 +101,8 @@ def import_module(name, package=None):
     The 'package' argument is required when performing a relative import. It
     specifies the package to use as the anchor point from which to resolve the
     relative import to an absolute import.
-
     """
+    
     if name.startswith('.'):
         if not package:
             raise TypeError("relative imports require the 'package' argument")
@@ -34,19 +112,25 @@ def import_module(name, package=None):
                 break
             level += 1
         name = _resolve_name(name[level:], package, level)
-    __import__(name)
+
+    # Reeplace builtin __import__
+    builtins.__import__ = _import
+    
+    if name in sys.modules:
+        reload_module(sys.modules[name])
+    else:
+        __import__(name)
+    
+    # Restore builtin __import__
+    builtins.__import__ = _baseimport
+    
     return sys.modules[name]
 
 def import_from_directory(directory, name):
-    """ 
-    Import a module from directory
-    """
-    #TODO: ver si el modulo ya esta importado, quiza con solo un reload alcanza
-    #TODO: ver como usar esta funcion con "with"
+    """Import a module from directory"""
     sys.path.insert(1, directory)
     try:
         module = import_module(name)
-        imp.reload(module) # Might be out of date
     except ImportError as reason:
         print(reason)
         raise reason
