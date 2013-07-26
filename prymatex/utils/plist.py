e51500066cf208e4bb9bfd2cd930110b28c38b9d
diegomvh <diegomvh@gmail.com>
2013-07-26 08:54:44 -0300

    Added __fixString, now can fix the string in readPlistFromString

--- prymatex/utils/plist.py
+++ prymatex/utils/plist.py
@@ -11,6 +11,22 @@ from . import six
 XMLPATTERN = re.compile("<(\w+)>(.*)<\/\w+>");
 
 # FIX, FIX, FIX
+def __fixString(sourceString):
+    fixedString = ""
+    sourceIndex = 0
+    if not six.PY3 and not isinstance(sourceString, unicode):
+        sourceString = sourceString.decode("utf-8")
+    for match in XMLPATTERN.finditer(sourceString):
+        if __shouldWrap(match.group(2)):
+            fixedString += sourceString[sourceIndex:match.start()]
+            item = b'<data>' + plistlib.Data(match.group(2).encode("utf-8")).asBase64() + b'</data>'
+            fixedString += item.decode("utf-8")
+            sourceIndex = match.end()
+    fixedString += sourceString[sourceIndex:]
+    if not six.PY3 and not isinstance(sourceString, unicode):
+        fixedString = fixedString.encode("utf-8")
+    return fixedString
+    
 def __fixItems(dictionary, applyFunction):
     return dict(map(lambda item: (applyFunction(item[0]), applyFunction(item[1])), dictionary.items()))
 
@@ -65,33 +81,25 @@ def readPlist(filePath):
     try:
         data = plistlib.readPlist(filePath)
     except Exception as e:
-        result = []
-        start = 0
-        data = open(filePath).read()
-        if not six.PY3:
-            data = data.decode("utf-8")
-        for match in XMLPATTERN.finditer(data):
-            if __shouldWrap(match.group(2)):
-                result.append(data[start:match.start()])
-                item = b'<data>' + plistlib.Data(match.group(2).encode("utf-8")).asBase64() + b'</data>'
-                result.append(item.decode("utf-8"))
-                start = match.end()
-        result.append(data[start:])
-        result = "".join(result)
-        if not six.PY3:
-            result = result.encode("utf-8")
-        data = plistlib.readPlistFromString(result)
+        # Solo si tiene error
+        with open(filePath) as plistFile:
+            data = plistlib.readPlist(__fixString(plistFile.read()))
     return __fixItems(data, __fixReadItem)
     
+def readPlistFromString(string):
+    try:
+        data = plistlib.readPlistFromString(string)
+    except Exception as e:
+        # Solo si tiene error
+        data = plistlib.readPlistFromString(__fixString(string))
+    return __fixItems(data, __fixReadItem)
+
 def writePlist(dictionary, filePath):
     plistlib.writePlist(__fixItems(dictionary, __fixWriteItem), filePath)
     
 def writePlistToString(dictionary):
     return plistlib.writePlistToString(__fixItems(dictionary, __fixWriteItem))
-    
-def readPlistFromString(string):
-    return plistlib.readPlistFromString(string)
-    
+
 if __name__ == "__main__":
     testFile = "/mnt/datos/workspace/Prymatex/prymatex/prymatex/share/Bundles/ShellScript.tmbundle/Commands/man.plist"
     datos = readPlist(testFile)
