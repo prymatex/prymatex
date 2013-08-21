ef909bcc5604d22c64f8cfd797da2b32a4766503
diegomvh <diegomvh@gmail.com>
2013-08-21 16:30:23 -0300

    Clean imports

--- prymatex/widgets/message.py
+++ prymatex/widgets/message.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import collections
 
 from prymatex.qt import QtCore, QtGui
-from prymatex.utils import html
 
 class PopupMessageWidget(QtGui.QLabel):
     ''' 
