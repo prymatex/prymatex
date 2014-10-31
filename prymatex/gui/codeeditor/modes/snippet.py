c12896025ea5fd1166d382f47e74f6b163d23045
Diego van Haaster <diegomvh@gmail.com>
2014-10-30 23:39:14 -0300

    Clean snippet mode

--- prymatex/gui/codeeditor/modes/snippet.py
+++ prymatex/gui/codeeditor/modes/snippet.py
@@ -41,7 +41,6 @@ class CodeEditorSnippetMode(CodeEditorBaseMode):
             self.charactersBefore = cursor.document().characterCount()
         else:
             self.processor.stop()
-        return False
 
     def __snippet_update_holder_after(self, event):
         if event.text():
@@ -73,7 +72,7 @@ class CodeEditorSnippetMode(CodeEditorBaseMode):
     
     def __snippet_end(self, event):
         self.processor.stop()
-        return False
+        return True
 
     def __snippet_navigation(self, event):
         cursor = self.editor.textCursor()
@@ -84,61 +83,15 @@ class CodeEditorSnippetMode(CodeEditorBaseMode):
                 self.processor.previousHolder()
             return True
         self.processor.stop()
-        return False
 
     def __snippet_backspace(self, event):
         cursor = self.editor.textCursor()
         holderStart, holderEnd = self.processor.currentPosition()
         if not cursor.hasSelection() and cursor.position() == holderStart:
             self.processor.stop()
-        return False
 
     def __snippet_delete(self, event):
         cursor = self.editor.textCursor()
         holderStart, holderEnd = self.processor.currentPosition()
         if not cursor.hasSelection() and cursor.position() == holderEnd:
             self.processor.stop()
-        return False
-        
-    def __snippet_edit(self, event):
-        cursor = self.editor.textCursor()
-        if event.text():
-
-            holderStart, holderEnd = self.processor.currentPosition()
-
-            holderPosition = cursor.selectionStart() - holderStart
-            positionBefore = cursor.selectionStart()
-            charactersBefore = cursor.document().characterCount()
-            
-            # Insert Text
-            self.editor.keyPressEvent(event)
-            positionAfter = cursor.position()
-            charactersAfter = cursor.document().characterCount()
-            length = charactersBefore - charactersAfter 
-            
-            # Capture Text
-            cursor.setPosition(holderStart)
-            cursor.setPosition(holderEnd - length, QtGui.QTextCursor.KeepAnchor)
-            selectedText = self.editor.selectedTextWithEol(cursor)
-            cursor.removeSelectedText()
-            
-            # Update holder
-            self.processor.setHolderContent(selectedText)
-
-            # Render
-            self.processor.render()
-            
-            if selectedText:
-                newHolderStart, _ = self.processor.currentPosition()
-                self.editor.setTextCursor(
-                    self.editor.newCursorAtPosition(
-                        newHolderStart + holderPosition + (positionAfter - positionBefore)
-                    )
-                )
-            # TODO Esto mejor no, asi se queda igual en el holder aunque no tenga nada
-            elif self.processor.nextHolder():
-                # The holder is killed
-                self.processor.selectHolder()
-
-            return True
-        return False
