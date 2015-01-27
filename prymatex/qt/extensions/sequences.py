ca7789f6a5a3af85ff0a45519c2a7c41406bf24f
Diego van Haaster <diegomvh@gmail.com>
2015-01-27 10:50:33 -0300

    Add context sequence

---
+++ prymatex/qt/extensions/sequences.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from prymatex.qt import QtGui
+from prymatex.qt.helpers import keybinding
+
+class ContextKeySequence(QtGui.QKeySequence):
+    pass
