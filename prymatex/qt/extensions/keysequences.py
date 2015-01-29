264b97bb8d89c55a49d8edede5025d66e0e20be5
Diego van Haaster <diegomvh@gmail.com>
2015-01-29 09:36:22 -0300

    New class ContextKeySequence

---
+++ prymatex/qt/extensions/keysequences.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from prymatex.qt import QtGui
+from prymatex.qt.helpers import keybinding
+
+class ContextKeySequence(QtGui.QKeySequence):
+    def __init__(self, context, name, default=None, description=None):
+        self._context = context
+        self._name = name
+        self._default = keybinding(name)
+        self._description = description
+        if self._default.isEmpty() and default:
+            self._default = QtGui.QKeySequence.fromString(default)
+        super(ContextKeySequence, self).__init__(self._default)
+
+    def name(self):
+        return self._name
+        
+    def default(self):
+        return self._default
+
+    def description(self):
+        return self._description
+
+    def context(self):
+        return self._context
+
+    def identifier(self):
+        return '%s.%s' % (self._context, self._name)
+
