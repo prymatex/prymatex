80ff3a02b3418eae2fa7cecddaf9b437c3b08bb4
Diego van Haaster <diegomvh@gmail.com>
2015-01-29 09:48:52 -0300

    camelcase to text in keysequence description

--- prymatex/qt/extensions/keysequences.py
+++ prymatex/qt/extensions/keysequences.py
@@ -4,12 +4,14 @@
 from prymatex.qt import QtGui
 from prymatex.qt.helpers import keybinding
 
+from prymatex.utils import text as textutils
+
 class ContextKeySequence(QtGui.QKeySequence):
     def __init__(self, context, name, default=None, description=None):
         self._context = context
         self._name = name
         self._default = keybinding(name)
-        self._description = description
+        self._description = description or textutils.camelcase_to_text(name)
         if self._default.isEmpty() and default:
             self._default = QtGui.QKeySequence.fromString(default)
         super(ContextKeySequence, self).__init__(self._default)
