29a1a2dc89f11b24c040b09f9d18773e215debeb
Diego van Haaster <diegomvh@gmail.com>
2015-03-12 00:32:32 -0300

    Clean spaces

--- prymatex/gui/codeeditor/processors/command.py
+++ prymatex/gui/codeeditor/processors/command.py
@@ -114,13 +114,13 @@ class CodeEditorCommandProcessor(CodeEditorBaseProcessor, CommandProcessorMixin)
     def discard(self, context, outputFormat = None):
         pass
 
-    def replaceSelectedText(self, context, outputFormat = None):
-        self.editor.updatePlainText(context.outputValue, cursor = self.inputWrapper)
+    def replaceSelectedText(self, context, outputFormat=None):
+        self.editor.updatePlainText(context.outputValue, cursor=self.inputWrapper)
 
-    def replaceDocument(self, context, outputFormat = None):
+    def replaceDocument(self, context, outputFormat=None):
         self.editor.updatePlainText(context.outputValue)
 
-    def replaceSelection(self, context, outputFormat = None):
+    def replaceSelection(self, context, outputFormat=None):
         print("replaceSelection")
 
     # ------------ Version 2
@@ -132,21 +132,22 @@ class CodeEditorCommandProcessor(CodeEditorBaseProcessor, CommandProcessorMixin)
         elif outputFormat == "snippet":
             self.insertAsSnippet(context)
 
-    def insertText(self, context, outputFormat = None):
+    def insertText(self, context, outputFormat=None):
         self.inputWrapper.insertText(context.outputValue)
 
-    def atCaret(self, context, outputFormat = None):
+    def atCaret(self, context, outputFormat=None):
         print("atCaret")
 
-    def afterInput(self, context, outputFormat = None):
+    def afterInput(self, context, outputFormat=None):
         print("afterInput")
 
-    def afterSelectedText(self, context, outputFormat = None):
+    def afterSelectedText(self, context, outputFormat=None):
         self.inputWrapper.setPosition(self.inputWrapper.selectionEnd())
         self.inputWrapper.insertText(context.outputValue)
 
-    def insertAsSnippet(self, context, outputFormat = None):
+    def insertAsSnippet(self, context, outputFormat=None):
         # Build Snippet
+        print(context.outputValue)
         snippet = self.editor.application().supportManager.buildAdHocSnippet(
             context.outputValue, context.bundleItem.bundle,
             tabTrigger = context.bundleItem.tabTrigger)
@@ -155,10 +156,10 @@ class CodeEditorCommandProcessor(CodeEditorBaseProcessor, CommandProcessorMixin)
             textCursor = self.textCursor,
             disableIndent = self.disableIndent)
 
-    def showAsHTML(self, context, outputFormat = None):
+    def showAsHTML(self, context, outputFormat=None):
         self.editor.browserDock.setRunningContext(context)
 
-    def showAsTooltip(self, context, outputFormat = None):
+    def showAsTooltip(self, context, outputFormat=None):
         message = html.escape(context.outputValue.strip())
         timeout = len(message) * 20
         if timeout > 2000:
@@ -172,14 +173,14 @@ class CodeEditorCommandProcessor(CodeEditorBaseProcessor, CommandProcessorMixin)
             frmt = outputFormat or "text", timeout = timeout,
             links = callbacks)
 
-    def toolTip(self, context, outputFormat = None):
+    def toolTip(self, context, outputFormat=None):
         self.showAsTooltip(context, outputFormat)
 
-    def createNewDocument(self, context, outputFormat = None):
+    def createNewDocument(self, context, outputFormat=None):
         editor= self.editor.window().addEmptyEditor()
         editor.setPlainText(context.outputValue)
 
-    def newWindow(self, context, outputFormat = None):
+    def newWindow(self, context, outputFormat=None):
         if outputFormat == "html":
             self.editor.browserDock.newRunningContext(context)
         elif outputFormat == "text":
@@ -187,6 +188,6 @@ class CodeEditorCommandProcessor(CodeEditorBaseProcessor, CommandProcessorMixin)
             editor = self.editor.window().addEmptyEditor()
             editor.setPlainText(context.outputValue)
 
-    def openAsNewDocument(self, context, outputFormat = None):
+    def openAsNewDocument(self, context, outputFormat=None):
         editor = self.editor.window().addEmptyEditor()
         editor.setPlainText(context.outputValue)
