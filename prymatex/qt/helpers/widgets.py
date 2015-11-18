211a32d8760e1e7a23f75b3f6c751963deaa2e51
Diego van Haaster <diegomvh@gmail.com>
2015-11-18 12:50:41 -0300

    Fix center widget helper

--- prymatex/qt/helpers/widgets.py
+++ prymatex/qt/helpers/widgets.py
@@ -3,12 +3,20 @@
 
 from prymatex.qt import QtWidgets
 
-def center_widget(widget, scale = None):
+def screen_geometry(widget):
+    desktop = QtWidgets.QApplication.desktop()
+    return desktop.availableGeometry(desktop.screenNumber(widget))
+
+def center_widget(widget, scale=None):
     """
     Center de widget in the screen
     Scale is a tuple with width and height ex: (0.7, 0.65)
     """
-    screen = QtWidgets.QDesktopWidget().screenGeometry()
+    geometry = screen_geometry(widget)
     if scale is not None:
-        widget.resize(screen.width() * scale[0], screen.height() * scale[1])
-    widget.move((screen.width() - widget.size().width()) / 2, (screen.height() - widget.size().height()) / 2)
+        widget.resize(geometry.width() * scale[0], geometry.height() * scale[1])
+    size = widget.size()
+    widget.move(
+        geometry.left() + ((geometry.width() - size.width()) / 2),
+        geometry.top() + ((geometry.height() - size.height()) / 2)
+    )
